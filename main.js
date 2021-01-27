0c0f63ec282dd3c60dd561e5e8372bfda882d7d5
Jeff Riley <1jsriley@gmail.com>
2021-01-26 21:38:01 -0600

    second codewars

--- main.js
+++ main.js
@@ -4,5 +4,16 @@
 function filter_list(l) {
     return l.filter(v => typeof v == "number")
    }
+//Return the quarter of the year by the month given only integers
 
-   
+const quarterOf = (month) => {
+    if (month > 0 && month <= 3) {
+      return 1
+    } else if (month > 3 && month <= 6) {
+      return 2
+    } else if (month > 6 && month <= 9) {
+      return 3
+    } else if (month > 9 && month <= 12) {
+      return 4
+    }
+  }
