79881a442eedcfd484ea67207756d04b0299a8da
Jeff Riley <1jsriley@gmail.com>
2021-01-26 21:57:28 -0600

    only a refactor

--- main.js
+++ main.js
@@ -35,3 +35,15 @@ const quarterOf = (month) => {
       return 'F'
     }
   }
+
+  // Refactor'd 
+  function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
+
+  
