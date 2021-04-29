1467aeea40a431cceb5d5eaaf236cf9b68b4baca
Jeff Riley <1jsriley@gmail.com>
2021-04-29 14:26:03 -0500

    refactor

--- main.js
+++ main.js
@@ -208,3 +208,10 @@ function apple(x) {
     return 'Help yourself to a honeycomb Yorkie for the glovebox.'
   }
 }
+
+//Refactored
+function apple(x) {
+  return x ** 2 > 1000
+    ? "It's hotter than the sun!!"
+    : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
