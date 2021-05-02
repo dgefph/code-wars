42e5aca4407b51bc64385ba2c2233357bfd07414
Jeff Riley <1jsriley@gmail.com>
2021-05-02 09:50:49 -0500

    adding code wars

--- main.js
+++ main.js
@@ -215,3 +215,10 @@ function apple(x) {
     ? "It's hotter than the sun!!"
     : 'Help yourself to a honeycomb Yorkie for the glovebox.'
 }
+
+//Next code
+function apple(x) {
+  return x ** 2 > 1000
+    ? "It's hotter than the sun!!"
+    : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
